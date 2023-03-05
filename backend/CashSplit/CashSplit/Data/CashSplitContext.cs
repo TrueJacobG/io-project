856fa54a5478052149744d5a6e9b34fed240a3f8
GGGeralt <Bartek.lipinski.pl@gmail.com>
2023-03-05 14:01:20 +0100

    dodanie :)

---
+++ backend/CashSplit/CashSplit/Data/CashSplitContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CashSplit.Models;
+
+namespace CashSplit.Data
+{
+    public class CashSplitContext : DbContext
+    {
+        public CashSplitContext (DbContextOptions<CashSplitContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CashSplit.Models.test> test { get; set; } = default!;
+    }
+}
