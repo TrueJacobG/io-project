5019b602bcaf708f11a84fc31412b37d0a947b4d
Jakub <jakubgradzewicz1309@gmail.com>
2023-03-20 10:37:01 +0100

    event pages - beta bersion

--- frontend/src/components/event/EventCard.tsx
+++ frontend/src/components/event/EventCard.tsx
@@ -4,8 +4,12 @@ import { Event } from "../../types/Event";
 const EventCard = ({ event }: { event: Event }) => {
   return (
     <div className="event-card">
-      <Link to={"/event/" + event.id_event}>{event.name}</Link>
-      <p>{event.description}</p>
+      <Link to={"/event/" + event.id_event} className="event-link">
+        <div className="event-box">
+          <h1 className="event-name event-text">{event.name}</h1>
+          <p className="event-desc event-text">{event.description}</p>
+        </div>
+      </Link>
     </div>
   );
 };
