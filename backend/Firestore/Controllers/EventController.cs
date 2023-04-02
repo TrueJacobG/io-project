f6fab7ef9b25650878afb605eba3e924f0439412
GGGeralt <Bartek.lipinski.pl@gmail.com>
2023-04-02 17:42:19 +0200

    expenses demo

--- backend/Firestore/Controllers/EventController.cs
+++ backend/Firestore/Controllers/EventController.cs
@@ -19,6 +19,9 @@ namespace Firestore.Controllers
         FirebaseAuthProvider auth = new FirebaseAuthProvider(new FirebaseConfig(System.IO.File.ReadAllLines("userConnection.txt")[0]));
         FirestoreDb firestoreDb = FirestoreDb.Create(System.IO.File.ReadAllText("databaseName.txt"));
 
+        private readonly string eventCollection = "event";
+        private readonly string expenseCollection = "expense";
+
 
         public EventController(ILogger<EventController> logger)
         {
@@ -26,51 +29,19 @@ namespace Firestore.Controllers
         }
 
 
-        //TODO: authentication of events (check for same name and uid)
 
-        [EnableCors("Policy1")]
-        [HttpPost]
-        [Route("event", Name = "add")]
-        public async Task<IActionResult> Add([FromBody] EventModel model)
-        {
-            _logger.LogInformation($"EventModel adding Attempt for {model.name}");
 
-            if (!ModelState.IsValid)
-            {
-                _logger.LogError($"Wrong data model for event addition for {model.name}");
-                return BadRequest(ModelState);
-            }
-            var user = auth.GetUserAsync(Request.Headers["authorization"]).Result;
-
-            CollectionReference events = firestoreDb.Collection("event");
-            Dictionary<string, object> data1 = new Dictionary<string, object>()
-            {
-                {"creator", user.LocalId},
-                {"description", model.description},
-                {"name", model.name},
-                {"user_email", user.Email},
-                {"add_date", Timestamp.GetCurrentTimestamp()},
-            };
-
-            ArrayList users = new ArrayList();
-            data1.Add("users", users);
-
-            var a = await events.AddAsync(data1);
-
-            return Ok(JsonConvert.SerializeObject(new { id_event = a.Id }));
-        }
-
-        //TODO: return as events, not as cramped json
+        #region event
         [EnableCors("Policy1")]
         [HttpGet]
-        [Route("event", Name = "getall")]
+        [Route("event", Name = "getEvents")]
         public async Task<IActionResult> GetEvents()
         {
             _logger.LogInformation($"EventModel get Attempt");
 
             var user = auth.GetUserAsync(Request.Headers["authorization"]).Result;
 
-            Query events = firestoreDb.Collection("event").WhereEqualTo("creator", user.LocalId);
+            Query events = firestoreDb.Collection(eventCollection).WhereEqualTo("creator", user.LocalId);
             QuerySnapshot snap = await events.GetSnapshotAsync();
 
 
@@ -98,18 +69,105 @@ namespace Firestore.Controllers
             return Ok(JsonConvert.SerializeObject(result));
         }
 
+        [EnableCors("Policy1")]
+        [HttpPost]
+        [Route("event", Name = "addEvent")]
+        public async Task<IActionResult> Add([FromBody] EventModel model)
+        {
+            _logger.LogInformation($"EventModel adding Attempt for {model.name}");
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError($"Wrong data model for event addition for {model.name}");
+                return BadRequest(ModelState);
+            }
+            var user = auth.GetUserAsync(Request.Headers["authorization"]).Result;
+
+            CollectionReference events = firestoreDb.Collection(eventCollection);
+            Dictionary<string, object> data1 = new Dictionary<string, object>()
+            {
+                {"creator", user.LocalId},
+                {"description", model.description},
+                {"name", model.name},
+                {"add_date", Timestamp.GetCurrentTimestamp()},
+            };
+
+            ArrayList users = new ArrayList();
+            ArrayList expenses = new ArrayList();
+
+            data1.Add("users", users);
+            data1.Add("expenses", expenses);
+
+            var a = await events.AddAsync(data1);
+
+            return Ok(JsonConvert.SerializeObject(new { id_event = a.Id }));
+        }
+
+        #endregion event
+
+
+        #region event/{id_event}
+        [EnableCors("Policy1")]
+        [HttpGet]
+        [Route("event/{id_event}", Name = "getEvent")]
+        public async Task<IActionResult> GetEvent(string id_event)
+        {
+            _logger.LogInformation($"EventModel get Attempt");
+
+            Console.WriteLine(id_event);
+
+            DocumentReference events = firestoreDb.Collection("event").Document(id_event);
+            DocumentSnapshot result = await events.GetSnapshotAsync();
+            string data = string.Empty;
+            if (result.Exists)
+            {
+                List<string> users = new List<string>();
+
+                foreach (string item in result.GetValue<string[]>("users"))
+                {
+                    users.Add(await Translator.GetMail(item));
+                }
+                data = JsonConvert.SerializeObject(new
+                {
+                    name = result.GetValue<string>("name"),
+                    description = result.GetValue<string>("description"),
+                    add_date = result.GetValue<Timestamp>("add_date"),
+                    users = users,
+                });
+            }
+            return Ok(data);
+        }
+
+        [EnableCors("Policy1")]
+        [HttpDelete]
+        [Route("event/{id_event}", Name = "deleteEvent")]
+        public async Task<IActionResult> Delete(string id_event)
+        {
+            _logger.LogInformation($"EventModel delete Attempt for {id_event}");
+
+
+            DocumentReference eventToDelete = firestoreDb.Collection("event").Document(id_event);
+
+            await eventToDelete.DeleteAsync();
+
+            return Ok(JsonConvert.SerializeObject(new { }));
+        }
+        #endregion event/{uid}
+
+
+        #region event/{id_event}/user
 
         [EnableCors("Policy1")]
         [HttpPost]
-        [Route("event/{id_event}/user", Name = "adduser")]
+        [Route("event/{id_event}/user", Name = "addUser")]
         public async Task<IActionResult> AddUser([FromBody] UserInEventModel userModel, string id_event)
         {
             _logger.LogInformation($"Attempt for adding user {userModel.user_email} in event {id_event}");
             string uid = await Translator.GetUid(userModel.user_email);
 
-            DocumentReference events = firestoreDb.Collection("event").Document(id_event);
+            DocumentReference eventToUpdate = firestoreDb.Collection(eventCollection).Document(id_event);
 
-            DocumentSnapshot snapshot = await events.GetSnapshotAsync();
+            DocumentSnapshot snapshot = await eventToUpdate.GetSnapshotAsync();
             List<string> users = new List<string>();
             if (snapshot.Exists)
             {
@@ -122,7 +180,7 @@ namespace Firestore.Controllers
                 { "users", users }
             };
 
-            await events.UpdateAsync(updates);
+            await eventToUpdate.UpdateAsync(updates);
 
             return Ok(JsonConvert.SerializeObject(new { }));
         }
@@ -130,13 +188,13 @@ namespace Firestore.Controllers
 
         [EnableCors("Policy1")]
         [HttpDelete]
-        [Route("event/{id_event}/user", Name = "deleteuser")]
+        [Route("event/{id_event}/user", Name = "deleteUser")]
         public async Task<IActionResult> DeleteUser([FromBody] UserInEventModel userModel, string id_event)
         {
             _logger.LogInformation($"Attempt for deleting user {userModel.user_email} in event {id_event}");
             string uid = await Translator.GetUid(userModel.user_email);
 
-            DocumentReference events = firestoreDb.Collection("event").Document(id_event);
+            DocumentReference events = firestoreDb.Collection(eventCollection).Document(id_event);
 
             DocumentSnapshot snapshot = await events.GetSnapshotAsync();
             List<string> users = new List<string>();
@@ -155,46 +213,108 @@ namespace Firestore.Controllers
 
             return Ok(JsonConvert.SerializeObject(new { }));
         }
+        #endregion event/{uid}/user
 
-        //event/:id_event/user
 
+        #region event/{id_event}/expense
         [EnableCors("Policy1")]
         [HttpGet]
-        [Route("event/{uid}", Name = "getone")]
-        public async Task<IActionResult> GetEvent(string uid)
+        [Route("event/{id_event}/expense", Name = "getExpenses")]
+        public async Task<IActionResult> GetExpenses(string id_event)
         {
-            _logger.LogInformation($"EventModel get Attempt");
 
-            Console.WriteLine(uid);
+            DocumentReference eventReference = firestoreDb.Collection(eventCollection).Document(id_event);
+            DocumentSnapshot eventData = await eventReference.GetSnapshotAsync();
 
-            DocumentReference events = firestoreDb.Collection("event").Document(uid);
-            DocumentSnapshot result = await events.GetSnapshotAsync();
-            string data = string.Empty;
-            if (result.Exists)
+            //expense[] -> {id_expense, name, description, type, cost, author, date}[]
+            List<ExpenseLoadModel> data = new List<ExpenseLoadModel>();
+
+            if (eventData.Exists)
             {
-                List<string> users = new List<string>();
 
-                foreach (string item in result.GetValue<string[]>("users"))
+                foreach (string item in eventData.GetValue<string[]>("expenses"))
                 {
-                    users.Add(await Translator.GetMail(item));
+                    DocumentSnapshot expenseData = await firestoreDb.Collection(expenseCollection).Document(item).GetSnapshotAsync();
+
+                    ExpenseLoadModel model = new ExpenseLoadModel(expenseData.Id, expenseData.GetValue<string>("name"), expenseData.GetValue<string>("description"),
+                       expenseData.GetValue<string>("type"), expenseData.GetValue<double>("cash"), await Translator.GetMail(expenseData.GetValue<string>("creator")), expenseData.GetValue<Timestamp>("add_date").ToDateTime().ToString());
+
+                    data.Add(model);
                 }
-                data = JsonConvert.SerializeObject(new
-                {
-                    name = result.GetValue<string>("name"),
-                    description = result.GetValue<string>("description"),
-                    add_date = result.GetValue<Timestamp>("add_date"),
-                    users = users,
-                });
-
             }
-
-            return Ok(data);
+            return Ok(JsonConvert.SerializeObject(new {expenses = data}));
         }
 
+        [EnableCors("Policy1")]
+        [HttpPost]
+        [Route("event/{id_event}/expense", Name = "addExpense")]
+        public async Task<IActionResult> AddExpense(string id_event, [FromBody] ExpenseSaveModel model)
+        {
+            
+            DocumentReference eventToUpdate = firestoreDb.Collection(eventCollection).Document(id_event);
 
-        //*event/:id_event/user 	
-        //body user_email
-        //post
-     
+            var user = auth.GetUserAsync(Request.Headers["authorization"]).Result;
+
+            CollectionReference expense = firestoreDb.Collection(expenseCollection);
+            Timestamp time = Timestamp.GetCurrentTimestamp();
+
+            Dictionary<string, object> data1 = new Dictionary<string, object>()
+            {
+                {"creator", user.LocalId},
+                {"description", model.description},
+                {"name", model.name},
+                {"type", model.type},
+                {"cash", model.cash },
+                {"add_date", time},
+            };
+
+            var a = await expense.AddAsync(data1);
+
+            DocumentSnapshot snapshot = await eventToUpdate.GetSnapshotAsync();
+            List<string> expenses = new List<string>();
+            if (snapshot.Exists)
+            {
+                expenses = snapshot.GetValue<List<string>>("expenses");
+            }
+            expenses.Add(a.Id);
+
+            Dictionary<string, object> updates = new Dictionary<string, object>
+            {
+                { "expenses", expenses }
+            };
+            await eventToUpdate.UpdateAsync(updates);
+
+            return Ok(JsonConvert.SerializeObject(new {id_expense = a.Id, author = user.Email, date = time.ToDateTime().ToString() }));
+        }
+
+        [EnableCors("Policy1")]
+        [HttpDelete]
+        [Route("event/{id_event}/expense", Name = "deleteExpense")]
+        public async Task<IActionResult> DeleteExpense(string id_event,[FromBody] ExpenseDeletionModel model)
+        {
+            _logger.LogInformation($"Attempt for deleting expense {model} in event {id_event}");
+
+            DocumentReference events = firestoreDb.Collection(eventCollection).Document(id_event);
+
+            DocumentSnapshot snapshot = await events.GetSnapshotAsync();
+            List<string> expenses = new List<string>();
+            if (snapshot.Exists)
+            {
+                expenses = snapshot.GetValue<List<string>>(expenseCollection);
+            }
+            expenses.Remove(model.id_expense);
+
+            Dictionary<string, object> updates = new Dictionary<string, object>
+            {
+                { "expenses", expenses }
+            };
+
+            await events.UpdateAsync(updates);
+            await firestoreDb.Collection("expenses").Document(model.id_expense).DeleteAsync();
+
+            return Ok(JsonConvert.SerializeObject(new { }));
+        }
+
+        #endregion event/{id_event}/expense
     }
 }
---
+++ backend/Firestore/Models/ExpenseLoadModel.cs
@@ -0,0 +1,34 @@
+﻿using Google.Cloud.Firestore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Firestore.Models
+{
+    public class ExpenseLoadModel
+    {
+        [Required]
+        public string id_expense { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public string type { get; set; }
+        [Required]
+        public double cash { get; set; }
+        [Required]
+        public string author { get; set; }
+        [Required]
+        public string date { get; set; }
+
+        public ExpenseLoadModel(string id_expense, string name, string description, string type, double cash, string author, string date)
+        {
+            this.id_expense = id_expense;
+            this.name = name;
+            this.description = description;
+            this.type = type;
+            this.cash = cash;
+            this.author = author;
+            this.date = date;
+        }
+    }
+}
