e184b4185705e01997e5cbf755b01100234f94bf
GGGeralt <Bartek.lipinski.pl@gmail.com>
2023-04-21 21:27:32 +0200

    added EventIdController

---
+++ backend/Firestore/Event/Id/EventIdController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Firestore.Event.Id
+{
+    public class EventIdController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
