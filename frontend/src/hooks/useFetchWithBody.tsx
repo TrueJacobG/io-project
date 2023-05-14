b8529836a663a32df09001d699b170f913316fea
TrueJacobG <jakubgradzewicz1309@gmail.com>
2023-05-14 20:48:47 +0200

    localhost and open host

--- frontend/src/hooks/useFetchWithBody.tsx
+++ frontend/src/hooks/useFetchWithBody.tsx
@@ -1,4 +1,10 @@
-const link: string = "http://192.168.50.81:7012/api/v1";
+let link: string;
+
+if (import.meta.env.LOCALHOST === undefined) {
+  link = "http://192.168.50.81:7012/api/v1";
+} else {
+  link = "http://localhost:7012/api/v1";
+}
 
 const useFetchWithBody = async (url: string, method: string, token: string, body: any) => {
   return fetch(link + url, {
