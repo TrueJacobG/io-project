47af32de828aec71e79a8db9dc6df8a0668ef09e
GGGeralt <Bartek.lipinski.pl@gmail.com>
2023-05-14 22:59:53 +0200

    small update

--- backend/Firestore/Program.cs
+++ backend/Firestore/Program.cs
@@ -12,13 +12,16 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Policy1",
         policy =>
         {
-            policy.WithOrigins("http://192.168.50.81:5173", "http://localhost:5173", "http://127.0.0.1:5173", "http://192.168.50.171:5173").AllowAnyHeader().AllowAnyMethod();
+            policy.WithOrigins("http://192.168.50.81:5173",
+                "http://192.168.50.81:7012", 
+                "http://localhost:5173", 
+                "http://127.0.0.1:5173", 
+                "http://192.168.50.171:5173").AllowAnyHeader().AllowAnyMethod();
         }) ;
 });
 
 
 builder.Services.AddAuthorization();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
@@ -37,7 +40,6 @@ app.UseHttpsRedirection();
 app.UseCors("Policy1");
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
