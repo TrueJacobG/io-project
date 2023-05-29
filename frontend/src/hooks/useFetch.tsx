c19f690759ff7cef266c4960d304dbc8fc67a401
Jakub <jakubgradzewicz1309@gmail.com>
2023-05-29 10:38:51 +0200

    not localhost host

--- frontend/src/hooks/useFetch.tsx
+++ frontend/src/hooks/useFetch.tsx
@@ -1,12 +1,10 @@
 let link: string;
 
-if (import.meta.env.VITE_LOCALHOST === undefined) {
-  link = "http://192.168.197.212:7012/api/v1";
-} else {
+if (import.meta.env.VITE_NOTLOCALHOST === undefined) {
   link = "http://localhost:7012/api/v1";
+} else {
+  link = "http://192.168.197.212:7012/api/v1";
 }
-
-link = "http://localhost:7012/api/v1";
 
 const useFetch = async (url: string, method: string, token: string) => {
   return fetch(link + url, {
